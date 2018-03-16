25ab729afbfaf3608a827010fd4d39ba9123ea80
test <!abc76925>
2018-03-16 17:45:22 +0900

    pep8 - use pylint, except docstring

--- run_server.py
+++ run_server.py
@@ -1,14 +1,14 @@
-import setLogger
 import logging
 import configparser
 import json
-import requests
 import traceback
+import requests
+import setLogger
 from aiohttp import web
 from chatbot import do_not_call_api
 
-config = configparser.ConfigParser()
-config.read('config.properties')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('config.properties')
 
 
 def main():
@@ -24,7 +24,8 @@ async def message_handle(request):
         message = (await request.post())['message']
         result = do_not_call_api.check_message(message)
         if result is None:
-            api_call_module = __import__(config['setSite']['apiCallModule'], fromlist=["detect_intent_texts"])
+            api_call_module = __import__(CONFIG['setSite']['apiCallModule'],
+                                         fromlist=["detect_intent_texts"])
             result = await api_call_module.detect_intent_texts([message])
             if any(result["action"].find(s) > -1 for s in ['outer_retrieve', 'outer_response']):
                 result["result"] = await web_hook(result)
@@ -48,7 +49,7 @@ async def web_hook(result):
         # url = ""
         # payload = {}
 
-        url = config['service']['service_url']+action[2]+"/"
+        url = CONFIG['service']['service_url']+action[2]+"/"
         html = requests.post(url, data=payload)
         logging.debug(html)
     except Exception as err:
